fc463373b3d44cf6f57aede9460672c2ffff78c6
Xubing Liang <liangxubing@gmail.com>
2017-09-11 16:14:50 +1000

    setup the project

---
+++ frontend/src/js/components/Layout.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { connect } from "react-redux"
+
+import { fetchUser } from "../actions/userActions"
+import { fetchTweets } from "../actions/tweetsActions"
+
+@connect((store) => {
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    tweets: store.tweets.tweets,
+  };
+})
+export default class Layout extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchUser())
+  }
+
+  fetchTweets() {
+    this.props.dispatch(fetchTweets())
+  }
+
+  render() {
+    const { user, tweets } = this.props;
+
+    if (!tweets.length) {
+      return <div> 
+      <h1>caonima</h1>
+      <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+      </div>
+    }
+
+    const mappedTweets = tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)
+
+    return <div>
+      <h1>{user.name}</h1>
+      <ul>{mappedTweets}</ul>
+    </div>
+  }
+}

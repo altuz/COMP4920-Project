2dbcb32b57e00c6a73cf7b6ff82a65c98de5bc43
Xubing Liang <liangxubing@gmail.com>
2017-10-06 22:02:10 +1100

    done home pages

--- frontend/src/js/containers/Discover/index.js
+++ frontend/src/js/containers/Discover/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 // import { connect } from "react-redux";
 import { getDiscover } from '../../actions/userActions.js';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 
+const option = {
+  onRowClick: function(row, columnIndex, rowIndex) {
+    console.log(row)
+    alert(`You click row id: ${row.game_name}, column index: ${columnIndex}, row index: ${rowIndex}`);
+  },
+};
 
 export default class Main extends React.Component {
   constructor (props) {
@@ -17,17 +24,57 @@ export default class Main extends React.Component {
   componentWillMount() {
     getDiscover()
     .then((res)=>{
-      console.log(res);
+      this.setState({
+        results:res.data.results,
+        fetched: true,
+      })
     })
   }
 
+  imageFormatter(cell,row){
+    return (
+        <img style={{height:35}} src={cell}/>
+    )
+  };
+
+  priceFormatter(cell,row){
+    if(cell){
+      return (
+          <div>
+            ${cell}
+          </div>
+
+      )
+    }
+    return (
+        <div>free</div>
+    );
+  };
+
+  rateFormatter(cell,row){
+    return(
+        <div>
+          {cell}%
+        </div>
+    )
+  }
 
   render() {
-    return(
-      <div >
-        <h1>discover</h1>
-        <h2>todo: grab data from steam and display on datatables</h2>
-      </div>
-    );
+    if(this.state.fetched){
+      console.log(this.state.results);
+      return(
+          <div >
+            <BootstrapTable data={this.state.results} options={ option } hover pagination>
+              <TableHeaderColumn dataField='image_url' dataFormat={this.imageFormatter} width = '90px' ></TableHeaderColumn>
+              <TableHeaderColumn isKey dataField='game_name' >Game Name</TableHeaderColumn>
+              <TableHeaderColumn dataField='publisher' width='200px' >Released By</TableHeaderColumn>
+              <TableHeaderColumn dataField='price' dataFormat={this.priceFormatter} width='80px'>Price</TableHeaderColumn>
+              <TableHeaderColumn dataField='average_rating' dataFormat={this.rateFormatter}>Rating</TableHeaderColumn>
+              <TableHeaderColumn dataField='num_player'>Owners</TableHeaderColumn>
+            </BootstrapTable>
+          </div>
+      );
+    }
+    return null;
   }
 }

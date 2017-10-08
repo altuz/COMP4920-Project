f0a54ff7e85a3aac5f4b648096cc734964b91cbb
Xubing Liang <liangxubing@gmail.com>
2017-10-08 20:21:56 +1100

    fix bug on game page

--- frontend/src/js/containers/Results/index.js
+++ frontend/src/js/containers/Results/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
@@ -40,20 +41,34 @@ export default class Results extends React.Component {
     );
   };
 
-  rateFormatter(cell,row){
-    return(
+  rateFormatter(cell,row) {
+    return (
         <div>
           {cell}%
         </div>
     )
   }
 
+  nameFormatter(cell,row,enumObject, index){
+    return (
+        <div>
+          <Link className='game_name' to ={{
+            pathname: `/games/${row.game_id}`,
+            state: {index}
+          }}>
+            {cell}
+          </Link>
+        </div>
+    )
+
+  }
+
   render() {
     return(
       <div>
-        <BootstrapTable data={this.props.results} options={ option } hover pagination>
+        <BootstrapTable data={this.props.results}  hover pagination>
           <TableHeaderColumn dataField='image_url' dataFormat={this.imageFormatter} width = '90px' ></TableHeaderColumn>
-          <TableHeaderColumn isKey dataField='game_name' >Game Name</TableHeaderColumn>
+          <TableHeaderColumn isKey dataField='game_name'  dataFormat={this.nameFormatter} width='300px'>Game Name</TableHeaderColumn>
           <TableHeaderColumn dataField='publisher' width='200px' >Released By</TableHeaderColumn>
           <TableHeaderColumn dataField='price' dataFormat={this.priceFormatter} width='80px'>Price</TableHeaderColumn>
           <TableHeaderColumn dataField='average_rating' dataFormat={this.rateFormatter}>Rating</TableHeaderColumn>

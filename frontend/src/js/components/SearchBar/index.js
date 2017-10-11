6ed85d709df8eb1584f05a3c8c87fcf4eb3be6df
Xubing Liang <liangxubing@gmail.com>
2017-10-12 02:11:42 +1100

    search by categories and genres done

--- frontend/src/js/components/SearchBar/index.js
+++ frontend/src/js/components/SearchBar/index.js
@@ -3,6 +3,13 @@ import { searchGame,clearResult } from '../../actions/userActions.js';
 import { Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
+import Select from 'react-select-2';
+import categories from './category_list.json';
+import genres from './genre_list.json';
+import { Button } from 'react-bootstrap';
+const category = categories.category_list;
+const genre = genres.genre_list;
+
 
 @connect((store)=>{
   return {
@@ -14,9 +21,14 @@ class SearchBar extends React.Component {
     super(props)
     this.state = {
       q:'',
+      test:'',
       fetched : false,
+      selected_category: [],
+      selected_genre:[],
     }
     this.isFetched = this.isFetched.bind(this);
+    this.handleSelect_category_Change = this.handleSelect_category_Change.bind(this);
+    this.handleSelect_genre_Change = this.handleSelect_genre_Change.bind(this);
   }
 
 
@@ -32,11 +44,23 @@ class SearchBar extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.dispatch(searchGame(this.state.q,this.isFetched));
+    console.log(this.state);
+    this.props.dispatch(searchGame(this.isFetched,this.state));
+  }
+
+  handleSelect_category_Change (selected_category) {
+    console.log('You\'ve selected:', selected_category);
+    this.setState({ selected_category });
+    console.log(this.state.selected_category);
+  }
+
+  handleSelect_genre_Change (selected_genre){
+    console.log('You\'ve selected:', selected_genre);
+    this.setState({ selected_genre });
+    console.log(this.state.selected_genre);
   }
 
   render () {
-    console.log(this.props.results);
     return (
       <div className='searchbar-container'>
         <form onSubmit={this.handleSubmit.bind(this)}>
@@ -46,7 +70,30 @@ class SearchBar extends React.Component {
             placeholder='Search By game name eg: dota'
             onChange={this.handleChange.bind(this)}
             name='q'
-            value={this.state.username} />
+            value={this.state.q} />
+          <div className='select-input'>
+            <Select
+              multi
+              placeholder="Select category"
+              simpleValue
+              options={category}
+              onChange={this.handleSelect_category_Change}
+              value={this.state.selected_category}
+            />
+            <div className='second-select'>
+              <Select
+                  multi
+                  placeholder="Select genre"
+                  simpleValue
+                  options={genre}
+                  onChange={this.handleSelect_genre_Change}
+                  value={this.state.selected_genre}
+              />
+            </div>
+          </div>
+          <div className="form-group search-btn">
+            <Button className='btn btn-primary btn-md' type="submit" name="Submit"> Search</Button>
+          </div>
         </form>
         {this.state.fetched &&
          <Redirect to={{

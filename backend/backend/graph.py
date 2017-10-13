99f333976760b0b138381e2f7a28327d8ab1024c
altuz.nicholas <altuz.nicholas@gmail.com>
2017-10-13 22:00:05 +1100

    built graph for recommend v2. NOT tested.

---
+++ backend/backend/graph.py
@@ -0,0 +1,87 @@
+# Bipartite graph between set of users and set of games
+class Graph:
+    # define empty graph
+    def __init__(self):
+        self.user_count = 0
+        self.game_count = 0
+        # dict to lookup user/games to their index in set
+        self.uid_lookup = {}
+        self.gid_lookup = {}
+        # set of games and users
+        self.g_nodes = []
+        self.u_nodes = []
+
+    # add user, returns true if added, false if user already in list
+    def add_user(self, uid):
+        if uid in self.uid_lookup:
+            return False
+        new_user = Node(True, uid)
+        self.u_nodes.append(new_user)
+        self.uid_lookup[uid] = self.user_count
+        self.user_count += 1
+        return True
+
+    # add game, returns true if added, false if game already in list
+    def add_game(self, gid):
+        if gid in self.gid_lookup:
+            return False
+        new_game = Node(False, gid)
+        self.g_nodes.append(new_game)
+        self.gid_lookup[gid] = self.game_count
+        self.game_count += 1
+        return True
+
+    # add connection between user and game
+    def connect_u_g(self, gid, uid, hrs):
+        # Check if user - game pair in graph
+        if uid not in self.uid_lookup:
+            self.add_user(uid)
+        if gid not in self.gid_lookup:
+            self.add_game(gid)
+        # Get their index in graph
+        g_idx = self.gid_lookup[gid]
+        u_idx = self.uid_lookup[uid]
+        # Get the actual nodes
+        g_node = self.g_nodes[g_idx]
+        u_node = self.u_nodes[u_idx]
+        # Create a new edge
+        g_u_edge = Edge(g_node, u_node, hrs)
+        # We don't have to check if connection exist since it is guaranteed to be unique
+        g_node.addEdge(g_u_edge)
+        u_node.addEdge(g_u_edge)
+        return True
+
+    # calculate stats for games
+    def games_hours_stats(self):
+        for game in self.g_nodes:
+            game.categorize_hours()
+
+class Node:
+    # define a new node
+    def __init__(self, nt, nid):
+        self.node_type = nt
+        self.node_id = nid
+        self.edges = []
+        self.hours_stat = []
+
+    def addEdge(self, edge):
+        self.edges.append(edge)
+
+    # don't call this function on a user.
+    # as it won't make sense
+    # this data is only useful across individual games
+    # To categorize users we have to do this for every game and cross check it with users
+    def categorize_hours(self):
+        sorted_edges = sorted(self.edges, key = lambda edge : edge.weight, reverse = False)
+        num_edges = len(sorted_edges)
+        # Convert it to rating based on every 20th percentile
+        for i in range(0, 5):
+            edge_idx = int((float(num_edges) / 5) * (i + 1))
+            self.hours_stat.append(sorted_edges[edge_idx].weight)
+
+class Edge:
+    # define an edge between two nodes
+    def __init__(self, g_node, u_node, hours):
+        self.game = g_node
+        self.user = u_node
+        self.weight = hours
